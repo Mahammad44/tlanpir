7639b17c29bdf846a0ac09db32b760de5884b020
mahammad44 <mahammadbincarlton@gmail.com>
2025-04-27 18:46:23 +0100

    updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use leptos::*;
+mod server;
 
-#[component]
-fn App(cx: Scope) -> impl IntoView {
-    view! { cx,
-        <h1>"Hello from Tlanpir!"</h1>
-    }
-}
+use axum::{Router};
+use std::net::SocketAddr;
 
-fn main() {
-    mount_to_body(|cx| view! { cx,  <App /> });
+#[tokio::main]
+async fn main() {
+    // Setup the server using the server module
+    let app = server::create_app();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Server running on http://{}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
